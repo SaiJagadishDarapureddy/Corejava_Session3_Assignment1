c3242d1c89c7790ae05cb47308b077da12d37608
SaiJagadishDarapureddy <sai.jagadish.d@gmail.com>
2016-06-09 16:17:39 +0000

    Add files via upload

---
+++ Sqrt_Cubrt.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+import java.util.Scanner;
+
+public class Sqrt_Cubrt {
+	
+	private static Scanner input;
+
+	public static void main(String[] args) {
+		double number;
+		input = new Scanner(System.in);
+		System.out.println("Enter the Number:" );  
+		number = input.nextDouble();
+System.out.println("SQUARE ROOT:" +Math.sqrt(number));
+System.out.println("SQUARE ROOT:" +Math.cbrt(number));
+	}
+	
+}
